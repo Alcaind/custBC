1e3f5020a68509e9df990ab0a17b287ce6461e23
SpDim <ioannina.spyros1986@gmail.com>
2018-02-21 18:23:03 +0200

    first commit

--- app/modules/users/ucontrollers.js
+++ app/modules/users/ucontrollers.js
@@ -4,44 +4,15 @@ angular.module('Users', [
     'MainComponents',
     'ui.bootstrap',
     'ApiModules',
-    'Authentication'
+    'Authentication',
+    'GlobalVarsSrvs'
 ])
-    .controller('UsersController', ['$scope', 'MakeModal', '$http', 'api', 'orderByFilter', 'AuthenticationService', function ($scope, MakeModal, $http, api, orderBy, AuthenticationService) {
+    .controller('UsersController', ['$scope', 'MakeModal', '$http', 'api', 'orderByFilter', 'AuthenticationService', 'globalVarsSrv', 'makeController', function ($scope, MakeModal, $http, api, orderBy, AuthenticationService, globalVarsSrv, makeController) {
 
         AuthenticationService.CheckCredentials();
 
-        $scope.dp = [];
-        $scope.item = {};
-        $scope.method = '';
-        $scope.baseURL = 'api/public/users';
-
-        $scope.getUsers = function () {
-            api.apiCall('GET', $scope.baseURL, function (results) {
-                $scope.dp = results.data;
-                $scope.totalItems = $scope.dp.length;
-            });
-        };
-
-        $scope.deleteUser = function (item) {
-            api.apiCall('DELETE', $scope.baseURL + "/" + item.id, function (results) {
-                $scope.dp.splice($scope.dp.indexOf(item), 1);
-                $scope.item = {};
-                MakeModal.generalInfoModal('sm', 'Info', 'info', 'User Deleted', 1)
-            });
-        };
-
-        $scope.propertyName = 'fname';
-        $scope.reverse = true;
-        $scope.sorttable = orderBy($scope.dp, $scope.propertyName, $scope.reverse);
-
-        $scope.sortBy = function (propertyName) {
-            $scope.reverse = (propertyName !== null && $scope.propertyName === propertyName)
-                ? !$scope.reverse : false;
-            $scope.propertyName = propertyName;
-        };
-
-
-        $scope.getUsers();
+        $scope.ctrl = makeController.mainController('/users', 'usersTableConf');
+        $scope.ctrl.init();
     }])
 
     .controller('ProfileController', ['$scope', '$routeParams', 'api', 'MakeModal', 'AuthenticationService', function ($scope, $routeParams, api, MakeModal, AuthenticationService) {
@@ -50,24 +21,13 @@ angular.module('Users', [
         $scope.ucategories = {};
         $scope.baseURL = 'api/public/users';
 
-        api.apiCall('GET', 'api/public/tms', function (results) {
-            $scope.tms = results.data;
-        });
-
-        api.apiCall('GET', 'api/public/userscategories', function (results) {
-            $scope.ucategories = results.data;
-        });
 
         if (!$routeParams.userId) {
             $scope.item = {
-                tm_id: "",
                 fname: "",
                 sname: "",
                 phone: "",
-                em_main: "",
-                em_sec: "",
-                em_pant: "",
-                cat_id: "",
+                email: "",
                 comments: "",
                 user: "",
                 hash: ""
